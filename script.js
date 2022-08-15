dc22fe40ff44252b66ea1c407a1fd3ff3955d0d8
Mustapha <mustapha.seghir@gmail.com>
2022-08-15 23:38:28 +0200

    Can add books to the library

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const main = document.querySelector('main');
 const btnRead = document.querySelector('.btn-read');
 const addBook = document.querySelector('header > button');
 const formSection = document.querySelector('.form-section');
+const form = document.querySelector('form');
 const blurredElt = Array.from(
   document.querySelectorAll('body > *:not(.form-section, script)')
 );
 let isFormDisplayed = false;
+let bookTitle = document.querySelector('#title');
+let bookAuthor = document.querySelector('#author');
+let bookPages = document.querySelector('#pages');
+let bookRead = document.querySelector('#read-checkbox');
+const formAddBookBtn = document.querySelector('.form-container > button');
 
 function Book(_title, _author, _pages, _read) {
   this.title = _title;
@@ -16,13 +22,16 @@ function Book(_title, _author, _pages, _read) {
   this.read = _read;
 }
 
-const book1 = new Book('Title', 'Author', 256, true);
-
 const addBookToLibrary = (book) => myLibrary.push(book);
 
 const addCardToLibrary = () => {
   // eslint-disable-next-line no-restricted-syntax
+  main.innerHTML = '';
   for (const book of myLibrary) {
+    let status = 'unread';
+    if (book.read) {
+      status = 'read';
+    }
     main.innerHTML += `<div class="card">
         <div class="card-container">
           <div class="card-top">
@@ -31,7 +40,7 @@ const addCardToLibrary = () => {
           </div>
           <div class="card-bottom">
             <p class="pages">${book.pages} pages</p>
-            <button type="button" class="btn-read">READ</button>
+            <button type="button" class="btn-read ${status}">${status}</button>
             <button type="button" class="btn-remove">REMOVE</button>
           </div>
         </div>
@@ -61,18 +70,29 @@ const displayForm = () => {
 
 const hideForm = () => {
   if (isFormDisplayed) {
-    formSection.style.display = 'none';
     unblurElt();
     isFormDisplayed = false;
+    form.reset();
+    formSection.style.display = 'none';
   }
 };
 
 addBook.addEventListener('click', displayForm);
 formSection.addEventListener('click', (event) => {
   if (event.target === formSection) {
+    console.log('oe');
     hideForm();
   }
 });
 
-addBookToLibrary(book1);
-addCardToLibrary();
+formAddBookBtn.addEventListener('click', () => {
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookRead.checked
+  );
+  addBookToLibrary(book);
+  addCardToLibrary();
+  hideForm();
+});
